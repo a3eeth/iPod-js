b82a731cfb45616d3d0101da177f7d4a7a7e5f06
axs <axs@cisi.org>
2020-01-02 00:05:20 +0530

    vanilla js added - unsure on js vs jq

---
+++ app.js
@@ -0,0 +1,17 @@
+// var menu = document.getElementById('menuButton');
+
+function menuClicked() {
+    console.log('menu')
+}
+function backClicked() {
+    console.log('back')
+}
+function selectClicked() {
+    console.log('select');
+}
+function forwardClicked() {
+    console.log('forward');
+}
+function playpauseClicked() {
+    console.log('play|pause');
+}
